7c3c3b4a80b9b36e83d7d76cc2e5577ab1713a75
Max <mmckenna@ucvts.org>
2019-01-03 15:44:54 +0000

    Added more dialogue

--- FP.py
+++ FP.py
@@ -2,10 +2,11 @@ print("Welcome to this game!")
 name = input("What's your name? ")
 print(f"{name}! What a great name!")
 print("You are free to do whatever you want in this game. Your choices throughout the story affect what is available to you later and change the paths that you can embark on. Are you ready to begin the game?")
-print("1: Yes")
-print("2: No")
+print("Yes")
+print("No")
 playgame = input()
-if playgame == 1:
+if playgame == "Yes":
     print("Great! Your story begins in high school...")
 else:
     print("Congratulations! The only way to win the game is not to play...")
+print("It's your first day of freshman year, and you get a pop quiz. You know your friend will get the answers right, and he's sitting right next to you. You probably won't get caught either. Do you take the chance and look at your friend's paper or take the L?")
